6dadd82ac6c4e3490a56d39aa9ef08b30251a01d
Vaibhav <coder8809@gmail.com>
2026-06-03 01:11:45 +0530

    improving the code continuously

--- backend/routes/metrics.js
+++ backend/routes/metrics.js
@@ -10,7 +10,7 @@ router.get("/report/:submission_id",async (req,res)=>{
     round(avg(p50_lat)::numeric,2) as avg_p50_latency,
     round(avg(p99_lat)::numeric,2) as avg_p99_latency,
     round(avg(accuracy)::numeric,2) as final_accuracy,
-    round((max(tps)*(avg(accuracy)/100.0))-(avg(p99_lat)*10)::numeric,0) as composite_score
+    round(((max(tps)*(avg(accuracy)/100.0))-(avg(p99_lat)*10))::numeric,0) as composite_score
     from metrics_trading_engine where submission_id=$1 group by submission_id;
     `; 
     const result=await tsClient.query(query,[req.params.submission_id]);
@@ -43,7 +43,7 @@ router.get("/leaderboard",async (req,res)=>{
     round(avg(m.p50_lat)::numeric,2) as avg_p50_latency,
     round(avg(m.p99_lat)::numeric,2) as avg_p99_latency,
     round(avg(m.accuracy)::numeric,2) as final_accuracy,
-    round((max(m.tps)*(avg(m.accuracy)/100.0))-(avg(m.p99_lat)*10)::numeric,0) as composite_score
+    round(((max(m.tps)*(avg(m.accuracy)/100.0))-(avg(m.p99_lat)*10))::numeric,0) as composite_score
     from metrics_trading_engine m
     join submissions s on m.submission_d=s.submission_id
     where m.recorded_at>=Now()-interval '24 hours'
